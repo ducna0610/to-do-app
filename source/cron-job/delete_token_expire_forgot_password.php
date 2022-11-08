86d4cdad60cb2106c144280916589aadc72fcd1c
ducna0610 <ducna0610@gmail.com>
2022-11-08 20:27:01 +0700

    fixed cron-job delete_token_expire

--- source/cron-job/delete_token_expire_forgot_password.php
+++ source/cron-job/delete_token_expire_forgot_password.php
@@ -3,9 +3,8 @@
 require '../connect.php';
 
 $sql = "DELETE FROM forgot_password WHERE day(create_at) != day(current_timestamp())";
+mysqli_query($connect, $sql);
 
 sendMail('ducna0610@gmail.com', 'Admin deeptry', 'Báo cáo Cron Job', '<h2>Đã thực hiện [delete token expire forgot password]</h2> <br/> <p>Chúc bạn 1 ngày tốt lành :3</p>');
-
-mysqli_query($connect, $sql);
 
 mysqli_close($connect);
