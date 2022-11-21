6cd5a2fc44feec2e7921e44c293a953b445855b5
ducna0610 <ducna0610@gmail.com>
2022-11-21 14:48:05 +0700

    modified path

--- source/admin/nav.php
+++ source/admin/nav.php
@@ -1,7 +1,7 @@
 <div class="container">
     <nav class="nav justify-content-end">
         <a href="../sign-in-up/sign-out.php" class="nav-link">Sign out</a>
-        <a href="../index.php" class="nav-link">To do list</a>
+        <a href="../" class="nav-link">To do list</a>
         <h1 style="color: pink;">ADMIN DASHBOARD </h1>
         <h1 style="color: gray">
             Welcome
