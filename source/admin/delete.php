9f09c56e01c4f5831302d87f2ede7cf1ab50f14d
ducna0610 <ducna0610@gmail.com>
2022-11-22 23:56:07 +0700

    fixed delete user

--- source/admin/delete.php
+++ source/admin/delete.php
@@ -4,11 +4,11 @@ require '../connect.php';
 
 $id_user = $_GET['id_user'];
 
-$sql = "DELETE Users
-WHERE id_user = $id_user";
+$sql = "DELETE FROM Users
+WHERE id_user = '$id_user'";
 
 mysqli_query($connect, $sql);
 
 mysqli_close($connect);
 
-header('location: admin.php');
+header('location: ./');
